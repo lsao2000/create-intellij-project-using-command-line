8dd86a052b3fc5dcaf243c3c788bfa0bc8738b79
lsao2000 <lahcenenligen@gmail.com>
2023-12-28 17:00:39 +0100

    run this file in terminal

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Enter name of the project"
+read project
+echo $(IdeaProjects)
+echo $(git clone https://github.com/lsao2000/emptty-repo.git)
+echo $(mv emptty-repo $project)
+echo $(intellij-idea-community $project)
+
